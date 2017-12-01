65dd430476277bd091556c81b57c3a1639a4fcf8
Nonus <nonus@nonus.net>
2017-12-01 05:27:49 +0100

    Ready for Day1

---
+++ Advent/DayTemplate/DayTemplate.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Windows.Forms;
+using Helper;
+using static System.Console;
+
+#endregion
+
+namespace Advent
+{
+    internal static class DayTemplate
+    {
+        private static void Main()
+        {
+            var input = Utilities.GetInput(typeof(DayTemplate).Name);
+
+            WriteLine($"First part answer: {input}");
+            Clipboard.SetText(input);
+
+            ReadKey();
+        }
+    }
+}
---
+++ Advent/Helper/Utilities.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System;
+using System.IO;
+
+#endregion
+
+namespace Helper
+{
+    public static class Utilities
+    {
+        private static readonly string InputsFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}Inputs");
+
+        public static string GetInput(string projectName)
+        {
+            var filepath = Path.Combine(InputsFolderPath, $"{projectName}.txt");
+
+            if (!File.Exists(filepath))
+                throw new FileNotFoundException($"Could not find {filepath}");
+
+            string input;
+            using (var fs = new StreamReader(filepath))
+                input = fs.ReadToEnd();
+
+            Console.WriteLine("------- Start of input -------");
+            Console.WriteLine(input);
+            Console.WriteLine("-------  End of input  -------");
+
+            return input;
+        }
+    }
+}
