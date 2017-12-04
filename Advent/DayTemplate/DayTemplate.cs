09157e501e0217031fbebef2c52a87728381d956
Nonus <nonus@nonus.net>
2017-12-04 09:18:08 +0100

    Improved the template a bit

--- Advent/DayTemplate/DayTemplate.cs
+++ Advent/DayTemplate/DayTemplate.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Linq;
 using System.Windows.Forms;
 using Helper;
@@ -14,14 +13,48 @@ namespace Advent
 {
     internal static class DayTemplate
     {
+        [STAThread]
         private static void Main()
         {
-            var input = Utilities.GetInput(typeof(DayTemplate).Name);
+            var input = Utilities.GetInput(typeof (DayTemplate).Name);
 
-            WriteLine($"First part answer: {input}");
-            Clipboard.SetText(input);
+            var part1Tests = new Dictionary<string, int>
+                             {
+                                 { "1", 2 }
+                             };
+
+            if (part1Tests.All(t => t.Key.TestResultOf(Part1) == t.Value))
+            {
+                var p1 = Part1(input);
+
+                WriteLine($"First part answer: {p1}");
+                Clipboard.SetText(p1.ToString());
+
+                var part2Tests = new Dictionary<string, int>
+                                 {
+                                     { "1", 2 }
+                                 };
+
+                if (part2Tests.All(t => t.Key.TestResultOf(Part2) == t.Value))
+                {
+                    var p2 = Part2(input);
+
+                    WriteLine($"Second part answer: {p2}");
+                    Clipboard.SetText(p2.ToString());
+                }
+            }
 
             ReadKey();
+        }
+
+        private static int Part1(string input)
+        {
+            return 0;
+        }
+
+        private static int Part2(string input)
+        {
+            return 0;
         }
     }
 }
