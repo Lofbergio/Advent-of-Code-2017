522c0eb5c7a702c1b728a353783f0651a8e023be
Nonus <nonus@nonus.net>
2017-12-07 15:42:38 +0100

    Day7 is now done, weren't able to clean it up as much as wanted to :star:

--- Advent/Day07/Day07.cs
+++ Advent/Day07/Day07.cs
@@ -2,8 +2,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Helper;
@@ -45,9 +45,21 @@ cntj (57)", "tknk" }
             WriteLine($"Part1 answer: {p1}");
             Clipboard.SetText(p1);
 
-            var part2Tests = new Dictionary<string, string>
+            var part2Tests = new Dictionary<string, int>
                              {
-                                 { "1", "" }
+                                 { @"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)", 60 }
                              };
 
 
@@ -56,41 +68,86 @@ cntj (57)", "tknk" }
 
             var p2 = Part2(input);
             WriteLine($"Part2 answer: {p2}");
-            Clipboard.SetText(p2);
+            Clipboard.SetText(p2.ToString());
 
             ReadKey();
         }
 
-        private static string Part1(string input)
+        private static string Part1(string input) => new ProgramManager(input).Root.Name;
+
+        private static int Part2(string input) => new ProgramManager(input).OverweightProgram.Item1;
+    }
+
+
+
+    public class ProgramManager
+    {
+        private readonly List<Program> _programs;
+
+        public readonly Program Root;
+        public Tuple<int, Program> OverweightProgram;
+
+        public ProgramManager(string input)
         {
-            var programs = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(line => new Program(line)).ToList();
-            var dependable = programs.Where(p => p.HoldingUp != null);
-            foreach (var dep in dependable)
-            {
-                if (dependable.All(p => !p.HoldingUp.Contains(dep.Name)))
-                    return dep.Name;
-            }
+            var lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            _programs = lines.Select(line => new Program(line)).ToList();
+
+            var parents = _programs.Where(p => p.ChildrenNames != null).ToList();
+
+            // Just find the only program that has children but is not a child of any other program
+            Root =  parents.First(dep => parents.All(p => p.ChildrenNames.All(c => c != dep.Name)));
+            
+            FillChildren(Root);
         }
 
-        private static string Part2(string input)
+        private Program FillChildren(Program parent)
         {
-            return "";
+            if (parent.ChildrenNames == null)
+                return parent;
+
+            foreach (var name in parent.ChildrenNames)
+            {
+                if (parent.Children == null)
+                    parent.Children = new List<Program>();
+
+                parent.Children.Add(FillChildren(_programs.First(c => c.Name == name)));
+            }
+
+            if (parent.Children == null || OverweightProgram != null)
+                return parent;
+
+            var totals = parent.Children.Select(c => c.TotalWeight).ToList();
+            if (totals.Max() == totals.Min())
+                return parent;
+
+            var overWeightProgram = parent.Children[totals.IndexOf(totals.Max())];
+            OverweightProgram = new Tuple<int, Program>(overWeightProgram.Weight - (totals.Max() - totals.Min()), overWeightProgram);
+
+            return parent;
         }
     }
 
     public class Program
     {
         public string Name { get; }
-        public int Position { get; }
-        public List<string> HoldingUp { get; }
+        public int Weight { get; }
+        public List<Program> Children { get; set; }
+        public int TotalWeight => Children?.Sum(c => c.TotalWeight) + Weight ?? Weight;
+
+        public readonly IEnumerable<string> ChildrenNames;
 
         public Program(string line)
         {
             var split = line.Replace(",", "").Split(' ');
             Name = split[0];
-            Position = int.Parse(split[1].Replace("(", "").Replace(")", ""));
+            Weight = int.Parse(Regex.Replace(split[1], @"\s+|\(|\)", ""));
             if (split.Length > 2)
-                HoldingUp = new List<string>(split.Skip(3));
+                ChildrenNames = split.Skip(3);
+        }
+
+        public override string ToString()
+        {
+            return $"#{Name} ({Weight}){(Children != null ? $" Total:{TotalWeight}" : "")}";
         }
     }
 }
